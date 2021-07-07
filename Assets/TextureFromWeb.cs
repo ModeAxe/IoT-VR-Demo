e82fe9bf833e3cbafdc0d6038dc53d6cbe49fc89
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-07-06 23:40:43 -0400

    download texture script

---
+++ Assets/TextureFromWeb.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class TexturesFromWeb : MonoBehaviour
+{
+
+    public InputField texUrlInput;
+
+    [System.Obsolete]
+    public void setTextureFromWeb()
+    {
+        string textureurl = texUrlInput.text;
+        StartCoroutine(DownloadTexture(textureurl));
+    }
+
+    [System.Obsolete]
+    IEnumerator DownloadTexture(string textureurl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(textureurl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+            this.gameObject.GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+    }
+}
