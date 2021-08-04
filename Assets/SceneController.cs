23c72d33e44d73910bb0ccbd8ceb5072acae9496
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-08-03 21:05:25 -0400

    scene manager changes

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vuplex.WebView;
+using System;
+using System.Timers;
 
 public class SceneController : MonoBehaviour
 {
     public GameObject lightSwitch;
-    public GameObject board;
+    public GameObject mainLight;
+    private Timer _buttonRefreshTimer = new Timer();
+    public WebViewPrefab _mainWebViewPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +22,36 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (UnityEngine.InputSystem.Keyboard.current.vKey.wasPressedThisFrame)
         {
-            board.GetComponent<TexturesFromWeb>().setTextureFromWeb();
+            mainLight.SetActive(mainLight.activeInHierarchy ? false : true);
+            spawnWebView();
+
         }
+
+    }
+
+    void spawnWebView()
+    {
+        Debug.Log("Hello Are You There?");
+        _mainWebViewPrefab = WebViewPrefab.Instantiate(0.6f, 0.3f);
+        _mainWebViewPrefab.transform.parent = transform;
+        _mainWebViewPrefab.transform.localPosition = new Vector3(2.9f, 3f, 0.4f);
+        _mainWebViewPrefab.transform.localEulerAngles = new Vector3(0, 180, 0);
+        _mainWebViewPrefab.Initialized += (initializedSender, initializedEventArgs) => {
+            //_mainWebViewPrefab.WebView.UrlChanged += MainWebView_UrlChanged;
+            _mainWebViewPrefab.WebView.LoadUrl("https://www.blender.org");
+        };
+
+    }
+    void MainWebView_UrlChanged(object sender, UrlChangedEventArgs eventArgs)
+    {
+
+        _setDisplayedUrl(eventArgs.Url);
+        _buttonRefreshTimer.Start();
+    }
+    void _setDisplayedUrl(string url)
+    {
 
     }
 }
