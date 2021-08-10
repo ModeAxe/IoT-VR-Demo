bc2f446c8a44a478030a8cfa425c72cd306c8d90
Elijah Temwani Zulu <modeaxe@Gmail.com>
2021-08-10 13:06:59 -0400

    imported Unity MQTT libraries

---
+++ Assets/MqttManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using M2MqttUnity;
+
+public class MqttManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,9 +10,8 @@ public class SceneController : MonoBehaviour
 {
     public GameObject lightSwitch;
     public GameObject mainLight;
-    private Timer _buttonRefreshTimer = new Timer();
-    public WebViewPrefab _mainWebViewPrefab;
-
+    private WebViewPrefab _mainWebViewPrefab;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +24,7 @@ public class SceneController : MonoBehaviour
         if (UnityEngine.InputSystem.Keyboard.current.vKey.wasPressedThisFrame)
         {
             mainLight.SetActive(mainLight.activeInHierarchy ? false : true);
-            _mainWebViewPrefab.WebView.ExecuteJavaScript("document.write('LIGHT SWITCH')");
+            _mainWebViewPrefab.WebView.ExecuteJavaScript("document.write('lightswitch')");
 
         }
 
@@ -34,24 +33,13 @@ public class SceneController : MonoBehaviour
     void spawnWebView()
     {
         Debug.Log("Hello Are You There?");
-        _mainWebViewPrefab = WebViewPrefab.Instantiate(0.6f, 0.3f);
+        _mainWebViewPrefab = WebViewPrefab.Instantiate(0.6f, 0.9f);
         _mainWebViewPrefab.transform.parent = transform;
         _mainWebViewPrefab.transform.localPosition = new Vector3(2.9f, 3f, 0.4f);
         _mainWebViewPrefab.transform.localEulerAngles = new Vector3(0, 180, 0);
         _mainWebViewPrefab.Initialized += (initializedSender, initializedEventArgs) => {
-            //_mainWebViewPrefab.WebView.UrlChanged += MainWebView_UrlChanged;
-            _mainWebViewPrefab.WebView.LoadUrl("https://www.blender.org");
+            _mainWebViewPrefab.WebView.LoadUrl("https://www.amcharts.com/demos/animated-gauge/");
         };
-
-    }
-    void MainWebView_UrlChanged(object sender, UrlChangedEventArgs eventArgs)
-    {
-
-        _setDisplayedUrl(eventArgs.Url);
-        _buttonRefreshTimer.Start();
-    }
-    void _setDisplayedUrl(string url)
-    {
 
     }
 }
